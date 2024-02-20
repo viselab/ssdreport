a5d15b3ffc443141139685af2fe319ac63d614ae
gssalv <62501975+gssalv@users.noreply.github.com>
2024-02-20 09:29:53 +0100

    Update generate_sssd_report_excel.py

--- generate_sssd_report_excel.py
+++ generate_sssd_report_excel.py
@@ -1,25 +1,20 @@
 import sys
 import xlsxwriter
 
-# Ottieni i valori dalla riga di comando
 hostname = sys.argv[1]
-sssd_status = sys.argv[2]
-sssd_value = sys.argv[3]
-output_file = f"{hostname}_sssd_report.xlsx"
+status = sys.argv[2]
+value = sys.argv[3]
+file_path = "/path/to/report/directory/" + hostname + "_sssd_report.xlsx"
 
-# Crea un nuovo workbook e un worksheet
-workbook = xlsxwriter.Workbook(output_file)
+workbook = xlsxwriter.Workbook(file_path)
 worksheet = workbook.add_worksheet()
 
-# Imposta le intestazioni
 worksheet.write('A1', 'Hostname')
 worksheet.write('B1', 'SSSD Configuration Status')
 worksheet.write('C1', 'SSSD Configuration Value')
 
-# Aggiungi i dati
 worksheet.write('A2', hostname)
-worksheet.write('B2', sssd_status)
-worksheet.write('C2', sssd_value)
+worksheet.write('B2', status)
+worksheet.write('C2', value)
 
-# Chiudi il workbook
 workbook.close()
