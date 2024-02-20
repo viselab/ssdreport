f5bfb3f773033ac41c8da24e282304035e65739e
gssalv <62501975+gssalv@users.noreply.github.com>
2024-02-20 11:55:41 +0100

    Update generate_sssd_report_excel.py

--- generate_sssd_report_excel.py
+++ generate_sssd_report_excel.py
@@ -7,26 +7,34 @@ sssd_status = sys.argv[2]
 sssd_values = sys.argv[3].split(',')  # Supponendo che i valori siano separati da virgole
 
 # Definisci il percorso del file Excel
-report_file = f"/root/report/{hostname}_sssd_report.xlsx"
+report_file = f"/root/report/{hostname}.ADGroup_report.xlsx"
 
 # Crea un nuovo file Excel e aggiungi un foglio di lavoro
 workbook = xlsxwriter.Workbook(report_file)
 worksheet = workbook.add_worksheet()
 
-# Imposta le intestazioni delle colonne e la formattazione
-header_format = workbook.add_format({'bold': True, 'font_color': 'blue'})
+# Imposta la formattazione delle intestazioni
+header_format = workbook.add_format({
+    'bold': True,
+    'align': 'center',       # Centra il testo
+    'valign': 'vcenter',     # Centra verticalmente
+    'bg_color': '#FFFF00',   # Sfondo giallo
+    'border': 1              # Aggiunge un bordo
+})
+
+# Scrivi le intestazioni delle colonne con la formattazione
 worksheet.write('A1', 'Hostname', header_format)
 worksheet.write('B1', 'AD Configuration Status', header_format)
 worksheet.write('C1', 'AD Group', header_format)
 
-# Scrivi il nome dell'host e lo stato della configurazione nelle prime celle
-worksheet.write('A2', hostname)
-worksheet.write('B2', sssd_status)
+# Scrivi il nome dell'host e lo stato della configurazione nelle prime celle con formattazione centrata
+worksheet.write('A2', hostname, header_format)
+worksheet.write('B2', sssd_status, header_format)
 
 # Scrivi ogni gruppo AD in una nuova riga
 start_row = 2
 for group in sssd_values:
-    worksheet.write('C' + str(start_row), group.strip())
+    worksheet.write('C' + str(start_row), group.strip(), header_format)
     start_row += 1
 
 # Imposta la larghezza delle colonne per migliorare la leggibilità
