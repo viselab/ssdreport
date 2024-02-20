753e590c48098433c45bfe23d6de9c3e831fe5c6
gssalv <62501975+gssalv@users.noreply.github.com>
2024-02-20 17:08:28 +0100

    Update ad_report_global.py

--- ad_report_global.py
+++ ad_report_global.py
@@ -2,11 +2,6 @@ import xlsxwriter
 import json
 import sys
 
-# Assicurati che almeno un argomento sia stato passato (la stringa JSON con i dati)
-if len(sys.argv) < 2:
-    print("Errore: Nessun dato fornito allo script.")
-    sys.exit(1)
-
 # Carica i dati JSON passati dal playbook
 data = json.loads(sys.argv[1])
 
@@ -22,13 +17,14 @@ header_format = workbook.add_format({
     'bold': True,
     'align': 'center',
     'valign': 'vcenter',
-    'bg_color': 'yellow',
-    'border': 1
+    'bg_color': '#00FFFF',  # RGB per ciano
+    'font_color': '#000000'  # RGB per nero
 })
 data_format = workbook.add_format({
     'align': 'center',
     'valign': 'vcenter',
-    'border': 1,
+    'bg_color': '#FFFF00',  # RGB per giallo
+    'font_color': '#000000',  # RGB per nero
     'text_wrap': True
 })
 
@@ -40,13 +36,7 @@ for col, header in enumerate(headers):
 # Scrivi i dati di ogni host nel foglio
 for row, line in enumerate(data, start=1):
     # Dividi ogni riga in base al separatore '|'
-    # Assicurati che la stringa di dati sia strutturata come previsto
-    try:
-        hostname, status, groups = line.split('|')
-    except ValueError:
-        print(f"Errore nella decodifica della riga: {line}")
-        continue  # Salta questa riga se ci sono problemi nella divisione
-
+    hostname, status, groups = line.split('|')
     worksheet.write(row, 0, hostname, data_format)
     worksheet.write(row, 1, status, data_format)
     worksheet.write(row, 2, groups, data_format)
@@ -54,7 +44,7 @@ for row, line in enumerate(data, start=1):
 # Imposta la larghezza delle colonne
 worksheet.set_column('A:A', 20)
 worksheet.set_column('B:B', 25)
-worksheet.set_column('C:C', 50)
+worksheet.set_column('C:C', 40)
 
 # Chiudi il workbook
 workbook.close()
