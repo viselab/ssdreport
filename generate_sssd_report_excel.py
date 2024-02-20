20b3b04ff6ce4a89ed78fd3a51caf6a43f74a390
gssalv <62501975+gssalv@users.noreply.github.com>
2024-02-20 10:57:17 +0100

    Update generate_sssd_report_excel.py

--- generate_sssd_report_excel.py
+++ generate_sssd_report_excel.py
@@ -7,7 +7,7 @@ sssd_status = sys.argv[2]
 sssd_values = sys.argv[3].split(',')  # Supponendo che i valori siano separati da virgole
 
 # Definisci il percorso del file Excel
-report_file = f"/root/report/{hostname}_sssd_report.xlsx"
+report_file = f"/root/report/{hostname}.ADGroup_report.xlsx"
 
 # Crea un nuovo file Excel e aggiungi un foglio di lavoro
 workbook = xlsxwriter.Workbook(report_file)
