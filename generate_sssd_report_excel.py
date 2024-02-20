21ac6a690f495b2c7023a01cc44ea06dba73e5bf
gssalv <62501975+gssalv@users.noreply.github.com>
2024-02-20 10:02:17 +0100

    Update generate_sssd_report_excel.py

--- generate_sssd_report_excel.py
+++ generate_sssd_report_excel.py
@@ -4,7 +4,7 @@ import xlsxwriter
 hostname = sys.argv[1]
 status = sys.argv[2]
 value = sys.argv[3]
-file_path = "/path/to/report/directory/" + hostname + "_sssd_report.xlsx"
+file_path = "/root/report" + hostname + "_sssd_report.xlsx"
 
 workbook = xlsxwriter.Workbook(file_path)
 worksheet = workbook.add_worksheet()
