df1cc057e36261ca1d33ad2a302c122792f1dfea
gssalv <62501975+gssalv@users.noreply.github.com>
2024-02-20 10:17:02 +0100

    Update generate_sssd_report_excel.py

--- generate_sssd_report_excel.py
+++ generate_sssd_report_excel.py
@@ -1,20 +1,27 @@
 import sys
 import xlsxwriter
 
+# Prendi i parametri dalla riga di comando
 hostname = sys.argv[1]
-status = sys.argv[2]
-value = sys.argv[3]
-file_path = "/root/report" + hostname + "_sssd_report.xlsx"
+sssd_status = sys.argv[2]
+sssd_values = sys.argv[3]
 
-workbook = xlsxwriter.Workbook(file_path)
+# Definisci il percorso del file Excel
+report_file = f"/root/report/{hostname}_sssd_report.xlsx"
+
+# Crea un nuovo file Excel e aggiungi un foglio di lavoro
+workbook = xlsxwriter.Workbook(report_file)
 worksheet = workbook.add_worksheet()
 
+# Imposta le intestazioni delle colonne
 worksheet.write('A1', 'Hostname')
 worksheet.write('B1', 'SSSD Configuration Status')
-worksheet.write('C1', 'SSSD Configuration Value')
+worksheet.write('C1', 'SSSD Configuration Values')
 
+# Aggiungi i dati nelle celle
 worksheet.write('A2', hostname)
-worksheet.write('B2', status)
-worksheet.write('C2', value)
+worksheet.write('B2', sssd_status)
+worksheet.write('C2', sssd_values)
 
+# Chiudi il file Excel
 workbook.close()
