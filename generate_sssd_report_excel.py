77b4d7a0fc39705b6e0117563496eabd28aac8da
gssalv <62501975+gssalv@users.noreply.github.com>
2024-02-20 11:48:49 +0100

    Update generate_sssd_report_excel.py

--- generate_sssd_report_excel.py
+++ generate_sssd_report_excel.py
@@ -7,7 +7,7 @@ sssd_status = sys.argv[2]
 sssd_values = sys.argv[3].split(',')  # Supponendo che i valori siano separati da virgole
 
 # Definisci il percorso del file Excel
-report_file = f"/root/report/{hostname}.ADGroup_report.xlsx"
+report_file = f"/root/report/{hostname}_sssd_report.xlsx"
 
 # Crea un nuovo file Excel e aggiungi un foglio di lavoro
 workbook = xlsxwriter.Workbook(report_file)
@@ -17,16 +17,17 @@ worksheet = workbook.add_worksheet()
 header_format = workbook.add_format({'bold': True, 'font_color': 'blue'})
 worksheet.write('A1', 'Hostname', header_format)
 worksheet.write('B1', 'AD Configuration Status', header_format)
-worksheet.write('C1', 'AD Group ', header_format)
+worksheet.write('C1', 'AD Group', header_format)
 
-# Aggiungi i dati nelle celle con formattazione
-data_format = workbook.add_format({'text_wrap': True})
+# Scrivi il nome dell'host e lo stato della configurazione nelle prime celle
 worksheet.write('A2', hostname)
 worksheet.write('B2', sssd_status)
-row = 2
-for value in sssd_values:
-    worksheet.write(row, 2, value, data_format)
-    row += 1
+
+# Scrivi ogni gruppo AD in una nuova riga
+start_row = 2
+for group in sssd_values:
+    worksheet.write('C' + str(start_row), group.strip())
+    start_row += 1
 
 # Imposta la larghezza delle colonne per migliorare la leggibilità
 worksheet.set_column('A:A', 20)
